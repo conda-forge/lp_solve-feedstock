440f845140ed008580a19672b6d8d842840c06aa
Julien Schueller <schueller@phimeca.com>
2023-10-17 13:44:54 +0200

    Update to 5.5.2.11

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,9 @@
 # see https://github.com/PADrend/ThirdParty
 cp ${RECIPE_DIR}/CMakeLists.txt ${RECIPE_DIR}/lpsolveConfig.cmake.in .
 
-mkdir build && cd build
-
-cmake \
+cmake -LAH -G "Ninja" \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
-  ..
-make install -j${CPU_COUNT}
+  -B build
+cmake --build build --target install --parallel ${CPU_COUNT}
 
